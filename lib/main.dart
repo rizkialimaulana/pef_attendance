0b9408f4b11ddcfb434ee5a8c66188b391896d0d
rizkialimaulana <maulanarizkiali@gmail.com>
2024-07-28 21:38:38 +0700

    Resolve firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:pef_attendance_system/auth/screens/login_screen.dart';
 import 'package:pef_attendance_system/main_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
